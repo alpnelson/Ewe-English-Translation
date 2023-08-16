1d3b79fb2da2dee83d20e5cb9d1a7517a07162b0
alpnelson <alpnelson5@gmail.com>
2023-08-16 03:53:48 -0400

    first commit

---
+++ WebPage/WebPage/src/main/java/com/company/Ewetranslation/EweTranslationApplication.java
@@ -0,0 +1,13 @@
+package com.company.Ewetranslation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EweTranslationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EweTranslationApplication.class, args);
+	}
+
+}
